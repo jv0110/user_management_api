866ba92a59d86d740ba17f0ab4e8b44b1954263c
Joao Vitor <joaovitor3592@gmail.com>
2021-04-26 00:48:03 -0300

    adding delete_user_test.js

---
+++ src/tests/routes/Users/delete_user_test.js
@@ -0,0 +1,84 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../index');
+const UsersModel = require('../../models/UsersModel');
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('Delete user', () => {
+  beforeEach(async () => {
+    await UsersModel.deleteMany({});
+    user = await UsersModel.create({
+      name: 'João Vitor',
+      email: 'joaovitor3592@gmail.com',
+      password: 'LegenDary123'
+    });
+    const res = await chai.request(server)
+    .post('/api/login')
+    .send({
+      email: 'joaovitor3592@gmail.com',
+      password: 'LegenDary123'
+    });
+    token = 'Bearer ' + res.body.token;
+  });
+  describe('Status 404', () => {
+    it('if user to be deleted is not found', async () => {
+      await UsersModel.deleteMany({});
+      const res = await chai.request(server)
+      .delete('/api/user/' + user._id)
+      .set({ 'Authorization': token });
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('msg').eql('User not found');
+    });
+  });
+  describe('Status 200', () => {
+    it('If the user is deleted', async () => {
+      const res = await chai.request(server)
+      .delete('/api/user/' + user._id)
+      .set({ 'Authorization': token });
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('msg').eql('User deleted');
+    });
+  });
+  describe('Status 401', () => {
+    it('if authorization token is not sent', (done) => {
+      chai.request(server)
+      .get('/api/users')
+      .send({})
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Invalid token');
+        done(err);
+      });
+    });
+    it('if token is not Bearer', (done) => {
+      chai.request(server)
+      .get('/api/users')
+      .set({ 'Authorization': "Uchiha 123hjgn7"})
+      .send({})
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Invalid token type');
+
+        done(err);
+      });
+    });
+    it('if token value is invalid', (done) => {
+      chai.request(server)
+      .get('/api/users')
+      .set({'Authorization': 'Bearer 3490u309j'})
+      .send({})
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Invalid token value');
+        done(err);
+      });
+    });
+  });
+});
